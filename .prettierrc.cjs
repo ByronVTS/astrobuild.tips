03a1d2410ee2f7b6d9d2c848e3f025ff3f128d84
Drend <byronthyto@gmail.com>
2024-04-16 09:40:50 -0500

    Clase 9 - Comfiguracion de TypeScript y Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+const { default: plugin } = require("tailwindcss");
+
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    useTabs: true,
+    plugins:[
+        require("prettier-plugin-astro")
+    ],
+    overrides:[
+        {
+            files: "**/*astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
